2950ce5788a13832a41d7bb884958cbee5edc3ae
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-08-31 22:55:04 +0200

    typing starts over when button clicked

--- typewriter.js
+++ typewriter.js
@@ -13,23 +13,24 @@ let typeSound = document.querySelector("#typekey1");
 let typeSound2 = document.querySelector("#typekey2");
 let spaceSound = document.querySelector("#typespace");
 let btn = document.querySelector("button");
+
 function initLoop() {
   console.log(len);
 
   document.querySelector(".typewritten").textContent = "";
-
+  iterator = 0;
   typeSound.currentTime = 0;
+  typeSound2.currentTime = 0;
+  spaceSound.currentTime = 0;
   btn.addEventListener("click", loop);
 }
 
 function loop() {
   console.log("loop", iterator);
-
+  btn.addEventListener("click", loop);
+  document.querySelector(".typewritten").textContent += text.charAt(iterator);
+  iterator++;
   if (iterator < maxNumberOfIterations) {
-    document.querySelector(".typewritten").textContent += text.charAt(iterator);
-    iterator++;
-
-    typeSound.play();
     if (text.charAt(iterator) === " ") {
       spaceSound.play();
     } else if (text.charAt(iterator) % 2 === 0) {
@@ -37,7 +38,14 @@ function loop() {
     } else {
       typeSound2.play();
     }
-  }
 
-  setTimeout(loop, Math.floor(Math.random() * 1000));
+    setTimeout(loop, Math.floor(Math.random() * 1000));
+  }
+  if (iterator >= maxNumberOfIterations) {
+    typeSound.pause();
+    typeSound2.pause();
+    spaceSound.pause();
+    iterator = 0;
+    document.querySelector("button").addEventListener("click", initLoop);
+  }
 }
